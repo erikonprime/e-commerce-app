2e69ec02d435dcd221e89c07721d0bf0e9585f43
erikonprime <eriks.kondratjevs@gmail.com>
2022-12-15 14:52:07 +0200

    refactor: change route path

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/', name: 'default_')]
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
